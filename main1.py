8437f1ef6e3b68ea16e92011cbd7e2367a0614ed
Rendel69 <egorvtomske12@gmail.com>
2024-11-05 18:57:04 +0300

    Homefile

--- main1.py
+++ main1.py
@@ -1,1 +1,16 @@
-print('Hello')
+number_1: int = 12
+numder_2 = 15.5
+print(number_1,
+      numder_2,
+      )
+string_1 = 'hello "world"'
+string_2 = "hello 'world'"
+string_3 = "hello \"world\""
+string_4 = "\n"
+print(
+    string_1,
+    string_4,
+    string_2,
+    string_4,
+    string_3
+)
